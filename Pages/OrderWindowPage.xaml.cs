f9ceb0d2cd4cf5230fe0059b9056783584bf17b9
EmreKuzey04 <emrekuzey88@gmail.com>
2025-08-26 16:54:28 +0300

    buton

--- Pages/OrderWindowPage.xaml.cs
+++ Pages/OrderWindowPage.xaml.cs
@@ -6,6 +6,7 @@ public partial class OrderWindowPage : ContentPage
     {
         InitializeComponent();
     }
+
     private void OnAutoFetchClicked(object sender, EventArgs e)
     {
         // TODO implement later
@@ -14,96 +15,112 @@ public partial class OrderWindowPage : ContentPage
     // Symbol and option selectors
     private void OnSymbolChanged(object sender, EventArgs e)
     {
-        // TODO implement later
+        if (sender is Picker picker && picker.SelectedIndex != -1)
+        {
+            var symbol = picker.Items[picker.SelectedIndex];
+            Console.WriteLine($"Selected symbol: {symbol}");
+        }
     }
 
     private void OnCallOptionCheckedChanged(object sender, CheckedChangedEventArgs e)
     {
-        // TODO implement later
+        if (e.Value) Console.WriteLine("Order type: Call");
     }
 
     private void OnPutOptionCheckedChanged(object sender, CheckedChangedEventArgs e)
     {
-        // TODO implement later
+        if (e.Value) Console.WriteLine("Order type: Put");
     }
 
     // Trigger price
     private void OnTriggerPriceTextChanged(object sender, TextChangedEventArgs e)
     {
-        // TODO implement later
+        Console.WriteLine($"Trigger price: {e.NewTextValue}");
     }
 
     private void OnIncreaseTriggerClicked(object sender, EventArgs e)
     {
-        // TODO implement later
+        if (this.FindByName<Entry>("TriggerEntry") is Entry entry && decimal.TryParse(entry.Text, out decimal value))
+        {
+            entry.Text = (value + 1).ToString("F2");
+        }
     }
 
     private void OnDecreaseTriggerClicked(object sender, EventArgs e)
     {
-        // TODO implement later
+        if (this.FindByName<Entry>("TriggerEntry") is Entry entry && decimal.TryParse(entry.Text, out decimal value))
+        {
+            entry.Text = (value - 1).ToString("F2");
+        }
     }
 
     // Strike and expiration
     private void OnStrikeChanged(object sender, EventArgs e)
     {
-        // TODO implement later
+        if (sender is Picker picker && picker.SelectedIndex != -1)
+        {
+            Console.WriteLine($"Strike: {picker.Items[picker.SelectedIndex]}");
+        }
     }
 
     private void OnExpirationChanged(object sender, EventArgs e)
     {
-        // TODO implement later
+        if (sender is Picker picker && picker.SelectedIndex != -1)
+        {
+            Console.WriteLine($"Expiration: {picker.Items[picker.SelectedIndex]}");
+        }
     }
 
     // Position size
     private void OnPositionTextChanged(object sender, TextChangedEventArgs e)
     {
-        // TODO implement later
+        Console.WriteLine($"Position size: {e.NewTextValue}");
     }
 
     private void OnPositionPresetClicked(object sender, EventArgs e)
     {
-        // TODO implement later 2000-5000-10000$
+        if (sender is Button btn && this.FindByName<Entry>("PositionEntry") is Entry entry)
+        {
+            entry.Text = btn.Text.Replace("$", "");
+        }
     }
-
-
 
     // Stop loss
     private void OnStopLossTextChanged(object sender, TextChangedEventArgs e)
     {
-        // TODO implement later
+        Console.WriteLine($"Stop loss: {e.NewTextValue}");
     }
 
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
-        
-    }
     private void OnStopLossPreset(object sender, EventArgs e)
     {
-        // TODO implement later   0.2-0.5-1.0 preset
+        if (sender is Button btn && this.FindByName<Entry>("StopLossEntry") is Entry entry)
+        {
+            entry.Text = btn.Text.Replace("$", "");
+        }
     }
 
     // Final action
     private void OnCreateOrderClicked(object sender, EventArgs e)
     {
-        // TODO implement later
+        Console.WriteLine("Order created!");
     }
+
     private void OnAddOrderClicked(object sender, EventArgs e)
     {
-     var page = new OrderWindowPage();
+        var page = new OrderWindowPage();
 
-            var win = new Window(page)
-            {
-                Width = 1720,  // initial size before handler
-                Height = 200,
-                Title = "Order"
-            };
+        var win = new Window(page)
+        {
+            Width = 1720,
+            Height = 200,
+            Title = "Order"
+        };
 
-            Application.Current?.OpenWindow(win);
+        Application.Current?.OpenWindow(win);
 
-            // enforce size and center after native handler is ready
-            _ = WindowUtil.ResizeAsync(win, 1720, 200, center: true, lockResize: false);
+        _ = WindowUtil.ResizeAsync(win, 1720, 200, center: true, lockResize: false);
     }
+
     // Cancel butonunda bu pencereyi kapat
     void OnCancelClicked(object sender, EventArgs e)
     {
