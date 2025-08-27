0ea8402cb0cca927147c530c21561d0554e5ddc1
EmreKuzey04 <emrekuzey88@gmail.com>
2025-08-27 10:43:16 +0300

    button activated

--- Pages/OrderWindowPage.xaml.cs
+++ Pages/OrderWindowPage.xaml.cs
@@ -1,5 +1,25 @@
 namespace ArcTriggerMAUI.Pages;
 
+public class Order
+{
+    public string Symbol { get; set; }
+    public decimal TriggerPrice { get; set; }
+    public string OrderType { get; set; } // Call / Put
+    public string OrderMode { get; set; } // MKT / LMT
+    public decimal Offset { get; set; }
+    public string Strike { get; set; }
+    public string Expiry { get; set; }
+    public decimal PositionSize { get; set; }
+    public decimal StopLoss { get; set; }
+    public decimal ProfitTaking { get; set; }
+    public bool AlphaFlag { get; set; }
+
+    public override string ToString()
+    {
+        return $"Symbol: {Symbol}, Trigger: {TriggerPrice}, Type: {OrderType}, Mode: {OrderMode}, Offset: {Offset}, Strike: {Strike}, Expiry: {Expiry}, Position: {PositionSize}, StopLoss: {StopLoss}, Profit: {ProfitTaking}, Alpha: {AlphaFlag}";
+    }
+}
+
 public partial class OrderWindowPage : ContentPage
 {
     public OrderWindowPage()
@@ -12,16 +32,21 @@ public partial class OrderWindowPage : ContentPage
         // TODO implement later
     }
 
-    // Symbol and option selectors
+    // Symbol Picker
     private void OnSymbolChanged(object sender, EventArgs e)
     {
         if (sender is Picker picker && picker.SelectedIndex != -1)
         {
             var symbol = picker.Items[picker.SelectedIndex];
             Console.WriteLine($"Selected symbol: {symbol}");
+
+            // Window baþlýðýna yansýt
+            if (this.Window != null)
+                this.Window.Title = symbol;
         }
     }
 
+    // Call / Put
     private void OnCallOptionCheckedChanged(object sender, CheckedChangedEventArgs e)
     {
         if (e.Value) Console.WriteLine("Order type: Call");
@@ -54,20 +79,33 @@ public partial class OrderWindowPage : ContentPage
         }
     }
 
-    // Strike and expiration
+    // Strike Picker
     private void OnStrikeChanged(object sender, EventArgs e)
     {
         if (sender is Picker picker && picker.SelectedIndex != -1)
         {
-            Console.WriteLine($"Strike: {picker.Items[picker.SelectedIndex]}");
+            var strike = picker.Items[picker.SelectedIndex];
+            Console.WriteLine($"Strike: {strike}");
+
+            if (this.Window != null)
+                this.Window.Title = strike;
+
+            picker.Title = strike; // Picker Title güncelle
         }
     }
 
+    // Expiry Picker
     private void OnExpirationChanged(object sender, EventArgs e)
     {
         if (sender is Picker picker && picker.SelectedIndex != -1)
         {
-            Console.WriteLine($"Expiration: {picker.Items[picker.SelectedIndex]}");
+            var expiry = picker.Items[picker.SelectedIndex];
+            Console.WriteLine($"Expiration: {expiry}");
+
+            if (this.Window != null)
+                this.Window.Title = expiry;
+
+            picker.Title = expiry; // Picker Title güncelle
         }
     }
 
@@ -102,24 +140,68 @@ public partial class OrderWindowPage : ContentPage
     // Final action
     private void OnCreateOrderClicked(object sender, EventArgs e)
     {
-        Console.WriteLine("Order created!");
+        var symbol = (StockPicker.SelectedIndex != -1) ? StockPicker.Items[StockPicker.SelectedIndex] : "";
+        var triggerPrice = decimal.TryParse(TriggerEntry.Text, out var t) ? t : 0;
+        var orderType = CallRadioButton.IsChecked ? "Call" : "Put";
+        var orderMode = MktRadioButton.IsChecked ? "MKT" : "LMT";
+        var offset = decimal.TryParse(OffsetEntry.Text, out var o) ? o : 0;
+        var strike = (StrikePicker.SelectedIndex != -1) ? StrikePicker.Items[StrikePicker.SelectedIndex] : "";
+        var expiry = (ExpPicker.SelectedIndex != -1) ? ExpPicker.Items[ExpPicker.SelectedIndex] : "";
+        var position = decimal.TryParse(PositionEntry.Text, out var p) ? p : 0;
+        var stopLoss = decimal.TryParse(StopLossEntry.Text, out var s) ? s : 0;
+        var profit = decimal.TryParse(ProfitEntry.Text, out var pr) ? pr : 0;
+        var alpha = AlphaCheck.IsChecked;
+
+        var order = new Order
+        {
+            Symbol = symbol,
+            TriggerPrice = triggerPrice,
+            OrderType = orderType,
+            OrderMode = orderMode,
+            Offset = offset,
+            Strike = strike,
+            Expiry = expiry,
+            PositionSize = position,
+            StopLoss = stopLoss,
+            ProfitTaking = profit,
+            AlphaFlag = alpha
+        };
+
+        Console.WriteLine("Order created: " + order.ToString());
+
+        // Ekrana yazdýr
+        if (this.FindByName<Label>("DisplayLabel") is Label label)
+        {
+            label.Text = order.ToString();
+        }
     }
+
     private void OnProfitPresetClicked(object sender, EventArgs e)
     {
-        //TODO IMPLEMENT LATER
+        if (sender is Button btn && this.FindByName<Entry>("ProfitEntry") is Entry entry)
+        {
+            entry.Text = btn.Text.Replace("%", "");
+            Console.WriteLine($"Profit taking set to {entry.Text}%");
+        }
     }
 
     private void OnTrailClicked(object sender, EventArgs e)
     {
-        //TODO IMPLEMENT LATER
+        Console.WriteLine("Invalidate action triggered");
     }
+
     private void OnBreakevenClicked(object sender, EventArgs e)
     {
-        //TODO IMPLEMENT LATER
+        Console.WriteLine("Breakeven action triggered");
     }
+
     private void OnOffsetPresetClicked(object sender, EventArgs e)
     {
-        //TODO IMPLEMENT LATER
+        if (sender is Button btn && this.FindByName<Entry>("OffsetEntry") is Entry entry)
+        {
+            entry.Text = btn.Text;
+            Console.WriteLine($"Offset set to {entry.Text}");
+        }
     }
 
     private void OnAddOrderClicked(object sender, EventArgs e)
